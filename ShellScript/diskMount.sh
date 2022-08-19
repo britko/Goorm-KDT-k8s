a8c71ada800ed89bcef35e8cf626640f42dda552
britko <britko@naver.com>
2022-08-19 14:41:54 +0900

    update dickMount.sh

--- ShellScript/diskMount.sh
+++ ShellScript/diskMount.sh
@@ -11,5 +11,5 @@ sudo mkdir /mnt/exhdd
 sudo chown $USER:$USER /mnt/exhdd
 
 # Auto mount settings
-uuid_sdb1=$(ls -l /dev/disk/by-uuid | grep sdb | awk '{print $9}')
-echo -e "# extended disk /dev/sdb1\nUUID=$(echo $uuid_sdb1) /mnt/exhdd ext4 errors=remount-ro 0 1" | sudo tee -a /etc/fstab
+UUID_sdb1=$(ls -l /dev/disk/by-uuid | grep sdb | awk '{print $9}')
+echo -e "# extended disk /dev/sdb1\nUUID=$UUID_sdb1 /mnt/exhdd ext4 errors=remount-ro 0 1" | sudo tee -a /etc/fstab
