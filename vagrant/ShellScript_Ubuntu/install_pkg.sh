7b52b1b993fae5fd9398df0e74be34b037ae1259
britko <britko@naver.com>
2022-08-24 18:31:32 +0900

    update install_pkg.sh

--- vagrant/ShellScript_Ubuntu/install_pkg.sh
+++ vagrant/ShellScript_Ubuntu/install_pkg.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # install packages
 sudo apt update
-sudo apt install -y openssh-server net-tools tree vsftpd
+sudo apt install -y openssh-server net-tools tree telnetd vsftpd isc-dhcp-server bind9 \
+nfs-kernel-server
