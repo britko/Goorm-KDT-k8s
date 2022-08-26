c2ff95a2e7670be06beb18e1de1c652133560bba
britko <britko@naver.com>
2022-08-26 17:10:36 +0900

    [+]add sql

---
+++ sql/insert_stdata.sql
@@ -0,0 +1,6 @@
+insert into st_info values (101, '이길동', 'Game');
+insert into st_info values (102, '김길동', 'Computer');
+insert into st_info values (103, '홍길동', 'Game');
+insert into st_grade values (101, 90, 80);
+insert into st_grade values (102, 70, 95);
+insert into st_grade values (103, 80, 65);
